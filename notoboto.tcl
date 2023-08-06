325364c665d8e05e287d258e249af9c04f377907
John Colagioia <john@colagioia.net>
2023-08-06 06:46:17 -0400

    Create stub file for the application

---
+++ notoboto.tcl
@@ -0,0 +1,10 @@
+#!/usr/bin/wish
+
+# Notoboto:  A note-taking application that can work with but migrates
+# away from Boost Note.
+
+package require Img
+package require json
+
+set configfile "~/.config/Miniboost.json"
+
