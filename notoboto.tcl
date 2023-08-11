c9e89545de6d5f7d7488aefcc6218b60b7d79905
John Colagioia <john@colagioia.net>
2023-08-11 06:28:26 -0400

    Populate folder list from root JSON file

--- notoboto.tcl
+++ notoboto.tcl
@@ -110,3 +110,8 @@ wm title . Notoboto
 wm geometry . ${width}x${height}+${x}+${y}
 wm iconphoto . -default icon48 icon512
 
+foreach {folder} $folders {
+  .fr.pnl.choose.topics.topic insert end [dict get $folder "name"]
+  .fr.pnl.choose.topics.topic itemconfigure end -foreground [dict get $folder "color"]
+}
+
