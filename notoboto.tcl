0702ffa13a78ad7353cff05c5c97418edb681787
John Colagioia <john@colagioia.net>
2023-12-19 06:16:22 -0500

    Highlight links on note keystroke

--- notoboto.tcl
+++ notoboto.tcl
@@ -122,6 +122,8 @@ wm title . Notoboto
 wm geometry . ${width}x${height}+${x}+${y}
 wm iconphoto . -default icon48 icon512
 
+# Bind actions to widgets
+bind .fr.pnl.notearea <KeyRelease> {detectLinks .fr.pnl.notearea}
 foreach {folder} $folders {
   .fr.pnl.choose.topics.topic insert end [dict get $folder "name"]
   .fr.pnl.choose.topics.topic itemconfigure end -foreground [dict get $folder "color"]
